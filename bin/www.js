fce6ab8c0dcefd16979926393ca74e1743c54a6e
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-09-04 15:52:05 +0000

    🚧 Construction: Entry Point In file www making debug work

--- bin/www.js
+++ bin/www.js
@@ -19,14 +19,14 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = http.createServer(app);// (req, res)=>{...}
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 
 server.listen(port);
-server.on('error', onError);
+server.on('error', onError); // callback
 server.on('listening', onListening);
 
 /**
@@ -86,5 +86,5 @@ function onListening() {
   var bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  debug('📢 Listening on ' + bind);
 }
