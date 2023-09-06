6fd92bfb069f2ba4b20a446d5db870297a8c2d01
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-09-06 16:51:39 +0000

    ⭐ Enhance: App Add author route to users router

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+// Cargando dependencias
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var debug = require('debug')('dwpcii:server');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Creando la instancia de express
 var app = express();
 
-// view engine setup
+// Configurando el motor de plantillas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Se establecen los middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// Crea un server de archivos estaticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Registro de Middlewares de aplicación
 app.use('/', indexRouter);
+// Activa "usersRourter" cuando se
+// solicita "/users"
 app.use('/users', usersRouter);
+// app.use('/author', (req, res)=>{
+//   res.json({mainDeveloper: "Ivan Rivalcoba"})
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
