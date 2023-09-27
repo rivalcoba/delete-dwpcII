e9f81190a50fe9134a928a32cf1b31ac53bdd9e0
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-09-27 17:12:07 +0000

    ⭐ Feature: Webpack Middleware

--- server/app.js
+++ server/app.js
@@ -6,12 +6,49 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var debug = require('debug')('dwpcii:server');
 
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// Setting Webpack Modules
+import webpack from 'webpack';
+import WebpackDevMiddleware from 'webpack-dev-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+// Importing webpack configuration
+import webpackConfig from '../webpack.dev.config';
+
 // Creando la instancia de express
 var app = express();
+
+// Get the execution mode
+const nodeEnviroment = process.env.NODE_ENV || 'production'
+
+// Deciding if we add webpack middleware or not
+if(nodeEnviroment === 'development'){
+  // Start Webpack dev server
+  console.log("🛠️ Ejecutando en modo desarrollo 🛠️");
+  // Adding the key "mode" with its value "development"
+  webpackConfig.mode = nodeEnviroment;
+  // Setting the dev server port to the same value as the express server
+  webpackConfig.devServer.port = process.env.PORT;
+  // Setting up the HMR (Hot Module Replacement)
+  webpackConfig.entry = [
+    "webpack-hot-middleware/client?reload=true&timeout=1000",
+    webpackConfig.entry
+  ];
+	// Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
+  // Creating the bundler
+  const bundle = webpack(webpackConfig);
+  // Enabling the webpack middleware
+  app.use( WebpackDevMiddleware(bundle, {
+    publicPath: webpackConfig.output.publicPath
+  }) );
+  //  Enabling the webpack HMR
+  app.use( WebpackHotMiddleware(bundle) );
+}else{
+  console.log("🏭 Ejecutando en modo producción 🏭");
+}
 
 // Configurando el motor de plantillas
 app.set('views', path.join(__dirname, 'views'));
