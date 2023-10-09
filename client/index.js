ebfbf956123066f4965cfc5dfce407fb3e2d7deb
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-10-09 15:33:30 +0000

    ⭐ Feature: Materialize CSS Add materialize CSS via npm

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
 // cargando los estilos
 import './styles/style.css';
+// Importando estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
+
 // Mensaje en la consola
 console.log('🎉 Estilos cargados correctamente 🎉');
