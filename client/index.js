62d466f9cb109d5e78418cb6bb7c516d593c3477
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-12-19 22:27:11 +0000

    🌟 Feat: Guia de estilos AirBnB

--- client/index.js
+++ client/index.js
@@ -1,30 +1,4 @@
 // Cargando estilos
-import './styles/style.css'
+import './styles/style.css';
 
-console.log("🎉 Client Server Working powered by webpack 🎉");
-
-// Codigo ES6
-// Default Parameters
-let show = (msg="No message given") => {
-  console.log(msg)
-}
-
-// Async Await
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000)
-  });
-}
-
-// Funcion asincrona
-async function asyncCall() {
-  console.log("Calling");
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-}
-
-// Llmar a la funcion asincrona
-asyncCall();
-show();
+console.log('🎉 Client Server Working powered by webpack 🎉');
