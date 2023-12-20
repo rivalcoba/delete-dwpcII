50a0dd3cafd4d2b4023b0b8b864ad9b0e357d896
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-12-20 14:06:04 +0000

    🌟 Feat: Materialize Css

--- client/index.js
+++ client/index.js
@@ -1,4 +1,11 @@
 // Cargando estilos
 import './styles/style.css';
+// Importando estilos de Materialize CSS
+import 'materialize-css/dist/css/materialize.css';
+// Importando scripts de Materialize
+import 'materialize-css/dist/js/materialize';
+
+// Inicializando Scripts de Materialize para interactividad
+M.AutoInit();
 
 console.log('🎉 Client Server Working powered by webpack 🎉');
