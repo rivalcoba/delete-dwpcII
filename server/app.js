d06514c7c95dfa4d19372ffdbc9161adbaaf5ea0
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2023-12-20 00:18:16 +0000

    🌟 Feat: Winston Logger

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ import path from 'path';
 // Ayuda al manejo de cookies
 import cookieParser from 'cookie-parser';
 // Maneja el log de peticiones http
-import logger from 'morgan';
+import morgan from 'morgan';
 
 // Importando las dependencias de webpack
 import webpack from 'webpack';
@@ -18,6 +18,9 @@ import usersRouter from './routes/users';
 import indexRouter from './routes/index';
 // Importando la configuración de webpack
 import webpackConfig from '../webpack.dev.config';
+
+// Impornting winston logger
+import log from './config/winston';
 
 const app = express();
 
@@ -57,7 +60,7 @@ if (nodeEviroment === 'developement') {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+app.use(morgan('dev', { stream: log.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -69,6 +72,7 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
+  log.info(`404 Pagina no encontrada 🤷‍♂️ ${req.method} ${req.originalUrl}`);
   next(createError(404));
 });
 
