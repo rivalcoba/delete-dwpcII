5668729b5098f7cfca9a405cf1dcdb74bc34cf2b
Ivan Rivalcoba <ivan.rivalcoba@gmail.com>
2024-05-06 19:01:19 +0000

    Validación en el front-end I

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -20,14 +20,23 @@ const addPost = (req, res) => {
   // Se le informa al cliente
   if (validationError) {
     log.info('Error al validar Project');
-    res.status(422).json(validationError);
-  } else {
-    // Si no hay errores de validacion
-    // se desestructura la informacion valida
-    const { validData: project } = req;
-    // Se le informa al cliente
-    res.status(200).json(project);
+    // Desestructurando los datos de validacion
+    const { value: project } = validationError;
+    // Se extraen los campos que fallaron en la validacion
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Crea una variable temporal para
+      // almacenar el objeto anterior
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    return res.status(422).render('project/addView', { project, errorModel });
   }
+  // Si no hay error de validacion
+  const { validData: project } = req;
+  // Contestamos información al cliente de que se ha cargado el proyecto
+  log.info('Se entrega al cliente la info del proyecto cargado');
+  return res.status(200).json(project);
 };
 
 export default {
